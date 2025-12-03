dfaeb671bd5c6c447a56dcc39606bc5423fbe3f4
IVan-Gfs <ivangfs2004@gmail.com>
2025-12-02 23:48:26 -0300

    Style: formolários de cadastro

--- panorama_front-end/src/components/User.tsx
+++ panorama_front-end/src/components/User.tsx
@@ -1,11 +1,16 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import '../assets/css/usuario/userLayout.css'
+
+
 
 export default function User(){
 
+    const navigate = useNavigate();
     return (
         <div id="LayoutLogin">
             <header id="headerLogin">
+               
+                <img className="goBack" src="../../public/imgs/arrow-back.png" alt="Voltar" onClick={() => navigate(-1)} />
                 <Link to="/inicio">
                     <img src="../../public/logo.svg" alt="logo" className="logo" />
                 </Link>
