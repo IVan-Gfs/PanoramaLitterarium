89b783391813ddf7d09ef5fa75389656be3d76f6
IVan-Gfs <ivangfs2004@gmail.com>
2026-04-18 16:00:52 -0300

    Fix: Prisma Service

--- panorama_back-end/src/prisma/prisma.service.ts
+++ panorama_back-end/src/prisma/prisma.service.ts
@@ -12,12 +12,12 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
 
     constructor(){
         const dbConfig = {
-        host: process.env.DATABASE_HOST || '127.0.0.1', // Use IP se 'localhost' falhar
+        host: process.env.DATABASE_HOST || '127.0.0.1', 
         user: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
         port: Number(process.env.DATABASE_PORT) || 3306,
-        connectionLimit: 5,
+        connectionLimit: 10,
         };
 
         
@@ -31,9 +31,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
         try {
             await this.$connect();
-            const result = await this.$queryRaw`SELECT 1`;
             Logger.log('Conexão estabelecida com sucesso!');
-            console.log('Resultado do teste:', result);
         } catch (error) {
             Logger.error(`Falha na conexão com o banco`)
             throw error;
